249452c03edfc592bc0b2393e3b1120d61111ef9
fayez666 <abdalluhfayez420@gmail.com>
2023-11-14 01:40:45 +0200

    print string

---
+++ printf_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * printf_string - printa a string.
+ * @val: argumen t.
+ * Return: the len of the string.
+ */
+
+int printf_string(va_list val)
+{
+	char *s;
+	int i, len;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+}
